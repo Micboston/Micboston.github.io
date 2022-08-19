3b2779aa27e218cc6739a57048a03b2e760d45bc
Micboston <61673031+Micboston@users.noreply.github.com>
2022-08-19 07:46:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function coder() {
- location.replace("https://dm5gsf.mimo.run/index.html")
+ location.replace("https://ecmbqa.mimo.run/index.html")
 }
 function heart() {
  location.replace("https://ecmbqa.mimo.run/index.html")
